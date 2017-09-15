3aedfa851c06a00e374feebeb6e4b1c4d1821eff
youyo <1003ni2@gmail.com>
2017-09-15 15:44:46 +0900

    create linux_extended.swap.size

--- linux-extended.go
+++ linux-extended.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"errors"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/drael/GOnetstat"
 	g2z "gopkg.in/cavaliercoder/g2z.v3"
@@ -12,6 +14,7 @@ import (
 func init() {
 	g2z.RegisterUint64Item("linux_extended.netstat.count", "LISTEN,tcp", linuxExtendedNetstatCount)
 	g2z.RegisterDiscoveryItem("linux_extended.swap.discovery", "", linuxExtendedSwapDiscovery)
+	g2z.RegisterUint64Item("linux_extended.swap.size", "/swap,used", linuxExtendedSwapSize)
 }
 
 func linuxExtendedNetstatCount(request *g2z.AgentRequest) (value uint64, err error) {
@@ -78,6 +81,43 @@ func countUp(data []GOnetstat.Process, state string) (value uint64) {
 }
 
 func linuxExtendedSwapDiscovery(request *g2z.AgentRequest) (d g2z.DiscoveryData, err error) {
+	f, err := os.Open("/proc/swaps")
+	defer f.Close()
+	if err != nil {
+		return
+	}
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		if !strings.HasPrefix(s.Text(), "Filename") {
+			device := strings.Fields(s.Text())[0]
+			d = append(d, g2z.DiscoveryItem{
+				"DEVICE": device,
+			})
+		}
+	}
+	return
+}
+
+func linuxExtendedSwapSize(request *g2z.AgentRequest) (value uint64, err error) {
+	device := ""
+	unit := "used"
+	switch len(request.Params) {
+	case 2:
+		if request.Params[0] != "" {
+			device = request.Params[0]
+		}
+		if request.Params[1] != "" {
+			unit = request.Params[1]
+		}
+	case 1:
+		if request.Params[0] != "" {
+			device = request.Params[0]
+		}
+	default:
+		err = errors.New("Undefine device")
+		return
+	}
+
 	var l []string
 	f, err := os.Open("/proc/swaps")
 	defer f.Close()
@@ -86,12 +126,28 @@ func linuxExtendedSwapDiscovery(request *g2z.AgentRequest) (d g2z.DiscoveryData,
 	}
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		l = append(l, s.Text())
+		if strings.HasPrefix(s.Text(), device) {
+			l = strings.Fields(s.Text())
+		}
 	}
-	if len(l) >= 2 {
-		d = append(d, g2z.DiscoveryItem{
-			"SWAP": "true",
-		})
+	total, err := strconv.ParseUint(l[2], 10, 64)
+	if err != nil {
+		return
+	}
+	used, err := strconv.ParseUint(l[3], 10, 64)
+	if err != nil {
+		return
+	}
+
+	switch unit {
+	case "used":
+		value = used
+	case "total":
+		value = total
+	case "free":
+		value = total - used
+	case "pfree":
+		value = used / total * 100
 	}
 	return
 }
